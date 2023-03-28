aeb7838ce0da2c695928ff98fd39fbd6b83a4d13
Derek Ronske <dronske@iu.edu>
2023-03-28 12:28:59 -0400

    lunch

---
+++ backend/OnCallDeveloperSolution/OnCallDeveloperApi.AcceptanceTests/GettingOnCallDeveloper.cs
@@ -0,0 +1,52 @@
+﻿using Alba;
+using OnCallDeveloperApi;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnCallDeveloperApi.Models;
+
+namespace OnCallDeveloperApi.AcceptanceTests;
+
+public class GettingOnCallDeveloper
+{
+    [Fact]
+    public async Task CanGetOnCallDeveloperDuringWeekdays()
+    {
+        await using var host = await AlbaHost.For<Program>();
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/oncalldeveloper");
+            api.StatusCodeShouldBeOk();
+        });
+        var expected = new OnCallDeveloperModel
+        {
+            Name = "Bob Smith",
+            Phone = "888-8888",
+            Email = "bob@company.com"
+        };
+        var actualResponse = response.ReadAsJson<OnCallDeveloperModel>();
+        Assert.Equal(expected, actualResponse);
+    }
+
+    [Fact]
+    public async Task CanGetOnCallDeveloperDuringWeekends()
+    {
+        await using var host = await AlbaHost.For<Program>();
+        var response = await host.Scenario(api =>
+        {
+            api.Get.Url("/oncalldeveloper");
+            api.StatusCodeShouldBeOk();
+        });
+        var expected = new OnCallDeveloperModel
+        {
+            Name = "House of Outsourced Support, Inc.",
+            Phone = "800 111-1111",
+            Email = "support@house-of-outsourced-support.com"
+        };
+        var actualResponse = response.ReadAsJson<OnCallDeveloperModel>();
+        Assert.Equal(expected, actualResponse);
+    }
+}
---
+++ backend/OnCallDeveloperSolution/OnCallDeveloperApi/Models/OnCallDeveloperModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OnCallDeveloperApi.Models;
+
+public record OnCallDeveloperModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
